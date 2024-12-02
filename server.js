eeddbbbdb11e9c5f15d197a9a908e77f265bd3b6
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-12-02 16:40:10 +0100

    added the object array and populated it with the contents

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Assegno la porta 3000 al server
 const PORT = 3000;
 
-// Creo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+// Creo il progetto base con una rotta base "/" che ritorna un testo semplice con scritto ”Server del mio blog”
 app.get('/', (req, res) => {
     res.send("<h1>Server del mio blog</h1>");
 });
@@ -16,12 +16,16 @@ app.get('/', (req, res) => {
 
 
 
-
+// Rotta di fallback: se la pagina non è stata trovata restituisco un messaggio 404 personalizzato
+app.get('*', (req, res) => {
+    // Se lo stato della richiesta non è stato trovato (risponde con un error 404) allora rispondi con il seguente messaggio
+    res.status(404).send("<h1>404 - Pagina non trovata</h1>");
+});
 
 // Metto il server in ascolto su localhost e sulla porta 3000
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
-    
+
 });
 
 
